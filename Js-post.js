1c0bc73b93b2cc28082c3ee7f036cf9e1dc506a5
Keanu Rivaldy <34697748+kendzgn@users.noreply.github.com>
2018-02-27 04:01:01 +0700

    Update Js-post.js

--- Js-post.js
+++ Js-post.js
@@ -23,7 +23,7 @@ var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
 if(x=="1"){xxx="/2018/02/memulai-bisnis-online.html"}
 if(x=="2"){xxx="/2018/02/membuat-shortener.html"}
-if(x=="3"){xxx="/2018/02/cara-membuat-tombol-like-untuk-bloggger.html}
+if(x=="3"){xxx="/2018/02/cara-membuat-tombol-like-untuk-bloggger.html"}
 a_to_ve[i].href="https://sflku.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
